81a5e72e4bb7a9ac051dc3054c8aef455dce30fe
sabs3 <sabinakhanal@hotmail.co.uk>
2015-10-11 19:05:38 +0100

    Java task 6

---
+++ src/Environment.java
@@ -0,0 +1,9 @@
+
+public class Environment {
+	public static void main(String[] args) {
+		Shark sharkobject = new Shark();
+		
+		System.out.println(sharkobject.Kind + " is an " + sharkobject.Family + "  Which lives in"
+				+ " in " + sharkobject.Habitat + " , hence it is " + sharkobject.Type);
+		}
+}
